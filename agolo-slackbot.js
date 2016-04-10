9100f97855a21d4e4f473a26e86db7c5829ee90d
Prem Ganeshkumar <ganeprem@gmail.com>
2016-04-10 14:03:46 -0400

    Single doc summary works

--- agolo-slackbot.js
+++ agolo-slackbot.js
@@ -1,23 +1,52 @@
 var validUrl = require('valid-url');
 var RtmClient = require('@slack/client').RtmClient;
+var RestClient = require('node-rest-client').Client;
+
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 var SlackSecret = require('./slack-secrets.js');
-
 var TOKEN = SlackSecret.slackToken();
+var AGOLO_URL = SlackSecret.agoloURL();
+
 var LOG_LEVEL = 'debug';
 
 var slackClient = new RtmClient(TOKEN, {logLevel: LOG_LEVEL});
+var restClient = new RestClient();
 
 var bot; // Track bot user .. for detecting messages by yourself
 
-var summarize = function(url) {
-	var result = "Here's a summary of " + url;
+// Summarize a given URL and call the given callback with the result
+var summarize = function(url, callback) {
+	var result = "Here's Agolo's summary of " + url + "\n";
 
+	var args = {
+		data: {
+			"coref":"true",
+			"summary_length":"3",
+			"articles":[
+    			{
+					"type":"article",
+					"url": url,
+					"metadata":{}
+				}
+			]},
+		headers: { "Content-Type": "application/json" }
+	};
 
+	restClient.post(AGOLO_URL, args, function (data, rawResponse) {
+		console.log(data);
+		var sentences = data.summary[0].sentences;
 
-	return result;
+		// Quote each line
+		for (var i = 0; i < sentences.length; i++) {
+			sentences[i] = ">" + sentences[i];
+		}
+
+		result = result + sentences.join("\n-\n");
+
+		callback(result);
+	});
 }
 
 slackClient.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
@@ -45,7 +74,9 @@ slackClient.on("message", function(message) {
     		for (var i = 0; i < matches.length; i++) {
     			var candidate = matches[i];
     			if (validUrl.isWebUri(candidate)) {
-    				slackClient.sendMessage(summarize(candidate), channel);
+    				summarize(candidate, function(result) {
+    					slackClient.sendMessage(result, channel);
+    				});
     			}
     		}
     	}
