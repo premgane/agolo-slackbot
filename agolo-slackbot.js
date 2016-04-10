c8878c97e3c9ac6134c946a95d6080329bd26e48
Prem Ganeshkumar <ganeprem@gmail.com>
2016-04-10 13:38:30 -0400

    Extract URL

--- agolo-slackbot.js
+++ agolo-slackbot.js
@@ -1,15 +1,24 @@
+var validUrl = require('valid-url');
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
 var SlackSecret = require('./slack-secrets.js');
 
-var TOKEN = SlackSecret.token();
+var TOKEN = SlackSecret.slackToken();
 var LOG_LEVEL = 'debug';
 
 var slackClient = new RtmClient(TOKEN, {logLevel: LOG_LEVEL});
 
 var bot; // Track bot user .. for detecting messages by yourself
+
+var summarize = function(url) {
+	var result = "Here's a summary of " + url;
+
+
+
+	return result;
+}
 
 slackClient.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
 	bot = rtmStartData.self.id;
@@ -25,12 +34,23 @@ slackClient.on("message", function(message) {
 
     var text = message.text;
     var channel = message.channel;
+    var attachments = message.attachments;
 
-    slackClient.sendMessage('this is a test message', channel, function callback(){});
+    if (text) {
+    	var urlRegex = /<([^\s]+)>/;
 
-    
-
-    // More goes here later..
+    	var matches = text.match(urlRegex);
+    	if (matches) {
+    		// Start at index 1 because 0 has the entire match, not just the group
+    		for (var i = 0; i < matches.length; i++) {
+    			var candidate = matches[i];
+    			if (validUrl.isWebUri(candidate)) {
+    				slackClient.sendMessage(summarize(candidate), channel);
+    			}
+    		}
+    	}
+    	
+    }
 });
 
 slackClient.start();
