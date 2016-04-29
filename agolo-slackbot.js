0bb4aecd40a1c1d8fc620a13676ed9d33433d69c
Prem Ganeshkumar <ganeprem@gmail.com>
2016-04-28 20:10:09 -0400

    Move Heroku HTTP server to separate file

--- agolo-slackbot.js
+++ agolo-slackbot.js
@@ -183,33 +183,8 @@ slackClient.on('message', function(message) {
 slackClient.start();
 
 
+// Heroku requires us to run a webserver and periodically ping it
 if (HEROKU) {
-  // To prevent Heroku from crashing us. https://github.com/slackhq/node-slack-client/issues/39
-  http = require('http');
-  handle = function(req, res) {return res.end('hit'); };
-
-  server = http.createServer(handle);
-
-  server.listen(process.env.PORT || 5000);
-
-  if (process.env.HEROKU_APP_URL) {
-    var URL = process.env.HEROKU_APP_URL;
-
-    if (URL.indexOf('http') != 0) {
-      URL = 'http://' + URL;
-    }
-
-    console.log('Heroku app URL: ' + URL);
-
-    var heartbeat = function() {
-      restClient.get(URL, function(){
-        console.log('heartbeat!');
-      });
-    };
-
-    heartbeat();
-
-    var HEARTBEAT_INTERVAL_MINS = 5;
-    setInterval(heartbeat, HEARTBEAT_INTERVAL_MINS * 60 * 1000);
-  }
+  var kickoffServer = require('./heroku-http-server.js');
+  kickoffServer();
 }
