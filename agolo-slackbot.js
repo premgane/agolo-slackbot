2a2425f356d34fcd4ac8a2a9ff61b9c75dd99cfd
Prem Ganeshkumar <ganeprem@gmail.com>
2017-05-09 10:35:44 -0400

    Check for null URL after URL-parser

--- agolo-slackbot.js
+++ agolo-slackbot.js
@@ -143,11 +143,11 @@ var shouldSummarize = function(message, candidate) {
 
   // Check our blacklist
   var url = parseDomain(candidate);
-  if(BLACKLISTED_SITES[candidate]
+  if(url === null || (BLACKLISTED_SITES[candidate]
     || BLACKLISTED_SITES[url.subdomain + '.' + url.domain + '.' + url.tld]
     || BLACKLISTED_SITES[url.subdomain + '.' + url.domain]
     || BLACKLISTED_SITES[url.domain + '.' + url.tld]
-    || BLACKLISTED_SITES[url.domain]) {
+    || BLACKLISTED_SITES[url.domain])) {
     console.log('Blacklisted site: ', url);
     return false;
   }
